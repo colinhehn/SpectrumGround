c36d632a30c02ccf96083d592fa11112a2b088fd
Colin Hehn <colin45h@yahoo.com>
2020-09-23 21:38:35 -0500

    Initial Commit

---
+++ SpectrumGround/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpectrumGround
+//
+//  Created by Tiger Coder on 9/23/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
