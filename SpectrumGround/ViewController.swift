60d3dfa31d24ed7048e1cfcc32e720691b05dc48
Colin Hehn <colin45h@yahoo.com>
2020-09-25 11:48:16 -0500

    added spectrum animation component

--- SpectrumGround/ViewController.swift
+++ SpectrumGround/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var bgColors = [UIColor.systemRed, UIColor.systemPink, UIColor.systemPurple, UIColor.systemBlue, UIColor.systemTeal, UIColor.systemGreen, UIColor.systemYellow, UIColor.systemOrange]
+    
+    var bgControl = 0
+    var backGround : UIColor = UIColor.systemBlue
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        backGround = self.view.backgroundColor!
+        
+        self.spectrumGround(ogBack: backGround)
+                   
+    }
+    
+    func stopSpectrum(_ sender: UIButton){
+        bgControl = 1
+        self.view.backgroundColor = backGround
     }
 
-
+    func spectrumGround(ogBack : UIColor) -> Void {
+        while (bgControl == 0) {
+            for color in bgColors {
+                UIView.animate(withDuration: 1, animations: { () -> Void in self.view.backgroundColor = color})
+            }
+        }
+    }
 }
-
